1d94171ba59135991ea7215ca2bd6b9893c7d36a
Takafumi Miyanaga <orangekame3.dev@gmail.com>
2025-06-02 16:50:10 +0900

    Update API endpoints and improve logging in QubexBackend (#49)

--- scripts/qubex_config_downloader.sh
+++ scripts/qubex_config_downloader.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-API_URL=${QDASH_API_URL:-http://localhost:5715/api}
+API_URL=${QDASH_API_URL:-http://localhost:6004/api}
 
 echo "Using QDash API base URL: ${API_URL}"
 
 echo "Downloading calibration note..."
 curl -X GET "${API_URL}/calibration/note" \
   -H 'accept: application/json' \
-  -H 'X-Username: orangekame3' | jq '.note' > qubex_config/calib_note.json
+  -H 'X-Username: admin' | jq '.note' > qubex_config/calib_note.json
 
 echo "Process complete. 'calib_note.json' has been created."
 
